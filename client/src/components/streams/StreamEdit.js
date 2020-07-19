4c33db039e9650585ac84c0362b3eeda45e83d13
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2020-07-19 17:06:08 +0530

    passed onSubmit as a callback to StreamForm

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
 
-const StreamEdit = () => {
-	return (
-		<div>StreamEdit</div>
-	);
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
+
+class StreamEdit extends React.Component {
+	
+	componentDidMount(){
+		this.props.fetchStream(this.props.match.params.id)
+	}
+
+	onSubmit = (formValues) => {
+		this.props.editStream(this.props.match.params.id, formValues)
+	}
+
+	render(){
+
+		if(!this.props.stream){
+			return <div className="f3">Loading...</div>
+		}
+
+		return (
+			<div>
+				<h3>Edit A Stream</h3>
+				<StreamForm 
+					initialValues={_.pick(this.props.stream, 'title', 'description')}
+					onSubmit={this.onSubmit}
+				/>
+			</div>
+		);
+	}
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+	return { stream : state.streams[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps, { fetchStream , editStream })(StreamEdit);
