765c227c3229a0107b05c4bbc6e00c7276beb551
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2020-07-21 13:00:09 +0530

    added title and description to streamshow

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const StreamShow = () => {
-	return (
-		<div>StreamShow</div>
-	);
+import { fetchStream } from '../../actions';
+
+class StreamShow extends React.Component {
+
+	componentDidMount(){
+		this.props.fetchStream(this.props.match.params.id)
+	}
+
+	render(){
+
+		if(!this.props.stream){
+			return <h4>Loading...</h4>
+		}
+
+		const { title, description } = this.props.stream
+
+		return (
+				<div>
+					<h2>{title}</h2>
+					<h5>{description}</h5>
+				</div>
+			);
+		}
+	
+	}
+
+const mapStateToProps = (state, ownProps) => {
+	return { stream : state.streams[ownProps.match.params.id] }
 }
 
-export default StreamShow;
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
