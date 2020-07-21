d274ba741fd48dfdc39ea468e3e0aa390a2426a3
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2020-07-22 01:07:42 +0530

    minor change

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,19 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import GoogleAuth from './GoogleAuth';
+import tv from '../assets/tv.png';
 
 const Header = () => {
 
 	return (
 		<div className="ui secondary pointing menu">
 			<Link to="/" className="item">
-				Streamy
+				<img src={tv} alt='tv'/>
 			</Link>
 
-			<div className="right menu">
-				<Link to="/" className="item">
-					All Streamers
-				</Link>
+			<div className="right menu pt3">
 				<GoogleAuth/>
 			</div>
 		</div>
